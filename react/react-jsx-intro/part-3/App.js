b66dc67d6d17ee9c883f12c9e92629c0caa5fd39
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-10 18:05:54 -0400

    Create App.js that renders at least three copies of the Person component on the page.

---
+++ react/react-jsx-intro/part-3/App.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div>
+            <Person name="Johnathan" age={25} hobbies={["Reading", "Swimming", "Coding"]} />
+            <Person name="Alice" age={15} hobbies={["Dancing", "Singing"]} />
+            <Person name="Bob" age={20} hobbies={["Cooking", "Gaming"]} />
+        </div>
+    );
+}
