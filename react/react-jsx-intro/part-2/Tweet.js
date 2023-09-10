c027ba6e156f23bd739c61df35eb8bd4b58ebaf0
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-10 17:54:36 -0400

    Create Tweet.js function component which takes as props the username of the user who wrote the tweet, the name of the user who wrote the tweet, the date of the tweet, and the message being tweeted

---
+++ react/react-jsx-intro/part-2/Tweet.js
@@ -0,0 +1,9 @@
+function Tweet(props) {
+    return (
+        <div className="tweet">
+            <h3>{props.username} ({props.name})</h3>
+            <p>{props.message}</p>
+            <small>{props.date}</small>
+        </div>
+    );
+}
