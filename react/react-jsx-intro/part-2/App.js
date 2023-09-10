298b8afa5886a8ce7fec3c7b0137fb6513a87b4c
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-10 17:55:36 -0400

    Create App.js which renders t least 3 tweets

---
+++ react/react-jsx-intro/part-2/App.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div>
+            <Tweet username="john_doe" name="John Doe" date="2023-09-10" message="This is my first tweet!" />
+            <Tweet username="jane_doe" name="Jane Doe" date="2023-09-09" message="Hello, world!" />
+            <Tweet username="alice" name="Alice" date="2023-09-08" message="React is awesome!" />
+        </div>
+    );
+}
